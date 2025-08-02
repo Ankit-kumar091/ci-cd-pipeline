16e2618ea83d06fafc73d4df3ec64b90767a3e5e
komal1209 <komalgupta1209@gmail.com>
2025-08-02 14:36:14 +0530

    Initial commit with first spring boot app CI/CD

---
+++ first-ci-cd-app/src/main/java/com/codingnfun/first_ci_cd_app/FirstCiCdAppApplication.java
@@ -0,0 +1,13 @@
+package com.codingnfun.first_ci_cd_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstCiCdAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstCiCdAppApplication.class, args);
+	}
+
+}
---
+++ first-ci-cd-app/src/test/java/com/codingnfun/first_ci_cd_app/FirstCiCdAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingnfun.first_ci_cd_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstCiCdAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
